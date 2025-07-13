ba8313d52f39fa47575c5ddc63c03b4c9ef94988
addy-can <aditya.velma@gmail.com>
2025-07-13 03:53:55 -0400

    server.ts+vercel.json

--- src/routes/scriptureRoutes.ts
+++ src/routes/scriptureRoutes.ts
@@ -14,6 +14,9 @@ const router = express.Router();
  *       200:
  *         description: Successful response
  */
+router.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'Vedic Shastra API is alive!' });
+});
 router.get('/', getAllScriptures);
 router.post('/', protect, addScripture); // Only authenticated users can add
 
